128b869e3caf19a998e0a7543ef9945fd9270961
hai <fhzhan@best-inc.com>
2020-11-11 14:03:34 +0800

    完整的一个node博客完成

--- index.js
+++ index.js
@@ -1,13 +1,84 @@
 const path = require('path')
 const express = require('express')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('connect-flash')
+const config = require('config-lite')(__dirname)
+const routes = require('./routes')
+const pkg = require('./package')
+const winston = require('winston')
+const expressWinston = require('express-winston')
+
 const app = express()
-const indexRouter = require('./routes/index')
-const userRouter = require('./routes/users')
 
-app.set('views', path.join(__dirname, 'views'))// 设置存放模板文件的目录
-app.set('view engine', 'ejs')// 设置模板引擎为 ejs
+app.locals.blog = {
+  title: pkg.name,
+  description: pkg.description
+}
 
-app.use('/', indexRouter)
-app.use('/users', userRouter)
+// 设置模板目录
+app.set('views', path.join(__dirname, 'views'))
+// 设置模板引擎为 ejs
+app.set('view engine', 'ejs')
 
-app.listen(3000)
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')))
+// session 中间件
+app.use(session({
+  name: config.session.key, // 设置 cookie 中保存 session id 的字段名称
+  secret: config.session.secret, // 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+  resave: true, // 强制更新 session
+  saveUninitialized: false, // 设置为 false，强制创建一个 session，即使用户未登录
+  cookie: {
+    maxAge: config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
+  },
+  store: new MongoStore({// 将 session 存储到 mongodb
+    url: config.mongodb// mongodb 地址
+  })
+}))
+// flash 中间件，用来显示通知
+app.use(flash())
+app.use(require('express-formidable')({
+  uploadDir: path.join(__dirname, 'public/img'), // 上传文件目录
+  keepExtensions: true// 保留后缀
+}))
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user
+  res.locals.success = req.flash('success').toString()
+  res.locals.error = req.flash('error').toString()
+  next()
+})
+// app.use(expressWinston.logger({
+//   transports: [
+//     new (winston.transports.Console)({
+//       json: true,
+//       colorize: true
+//     }),
+//     new winston.transports.File({
+//       filename: 'logs/success.log'
+//     })
+//   ]
+// }))
+// 路由
+routes(app)
+// app.use(expressWinston.errorLogger({
+//   transports: [
+//     new winston.transports.Console({
+//       json: true,
+//       colorize: true
+//     }),
+//     new winston.transports.File({
+//       filename: 'logs/error.log'
+//     })
+//   ]
+// }))
+
+// 监听端口，启动程序
+// app.use(function (err, req, res, next) {
+//   console.error(err)
+//   req.flash('error', err.message)
+//   res.redirect('/posts')
+// })
+app.listen(config.port, function () {
+  console.log(`${pkg.name} listening on port ${config.port}`)
+})
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/', function (req, res) {
-  res.send('hello, express')
-})
-
-module.exports = router
+module.exports = function (app) {
+  app.get('/', function (req, res) {
+    res.redirect('/posts')
+  })
+  app.use('/signup', require('./signup'))
+  app.use('/signin', require('./signin'))
+  app.use('/signout', require('./signout'))
+  app.use('/posts', require('./posts'))
+  app.use('/comments', require('./comments'))
+  app.use(function (req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+  })
+  app.use(function(err,req, res,next) {
+    res.status(404).render('404')
+  })
+}
